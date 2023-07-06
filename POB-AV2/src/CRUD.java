2361782b0885352cd60e02a9f20f373114c69852
Mylena <102628246+MylenaAndrade@users.noreply.github.com>
2023-07-06 12:00:58 -0300

    Criando as funcionalidades no metodo CRUD

--- POB-AV2/src/CRUD.java
+++ POB-AV2/src/CRUD.java
@@ -1,2 +1,58 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class CRUD {
+    public static void ClienteInserir(ArrayList<Cliente> clientes, Cliente cliente){
+        clientes.add(cliente);
+    }
+    public static void ClienteRemover(ArrayList<Cliente> clientes, int id){
+        Iterator<Cliente> iterator = clientes.iterator();
+        while (iterator.hasNext()) {
+            Cliente cliente = iterator.next();
+            if (cliente.getId() == id) {
+                iterator.remove();
+            }
+        }
+    }
+
+    public static void ClienteAlterar(ArrayList<Cliente> clientes, int id, String novoNome, String novoEndereco, String novoPostalCode, String novoPais, String
+            novoCPF, String novoPassaporte, String novadtNascimento){
+        String nulo = "null";
+        for (Cliente produto : clientes) {
+            if (produto.getId() == id) {
+                if(!novoNome.equals(nulo)){
+                    produto.setNome(novoNome);
+                }
+                if(!novoEndereco.equals(nulo)){
+                    produto.setEndereco(novoEndereco);
+                }
+
+                if(!novoPostalCode.equals(nulo)){
+                    produto.setPostalCode(novoPostalCode);
+                }
+
+                if(!novoPais.equals(nulo)){
+                    produto.setPais(novoPais);
+                }
+
+                if(!novoCPF.equals(nulo)){
+                    produto.setCpf(novoCPF);
+                }
+
+                if(!novoPassaporte.equals(nulo)){
+                    produto.setPassaporte(novoPassaporte);
+                }
+                if(!novadtNascimento.equals(nulo)){
+                    produto.setCpf(novadtNascimento);
+                }
+                break;
+            }
+        }
+
+        public static void ClienteListar(ArrayList<Cliente> clientes){
+            for (Cliente produto: clientes) {
+                System.out.println("Nome: " + produto.getNome() + "\n" + "Descrição " + produto.getDescricao() + "\n" + "Preço por Kg: " + produto.getPreco() + " " + produto.getPeso() + "Kg" + "\n");
+            }
+        }
+    }
 }
--- POB-AV2/src/Main.java
+++ POB-AV2/src/Main.java
@@ -1,6 +1,69 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+        int opcao = 0;
+        int id=0;
+        String nome, endereco, postalCode, pais, cpf, passaporte,dtNascimento;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("Digite: \n[1]Inserir\n[2]Remover\n[3]Alterar\n[4]Listar\n[5]Sair");
+            opcao = sc.nextInt();
+            switch (opcao){
+                case 1:
+                    System.out.println("Digite o id: ");
+                    id = sc.nextInt();
+                    System.out.println("Digite o seu Nome: ");
+                    nome = sc.next();
+                    System.out.println("Endereço: ");
+                    endereco = sc.next();
+                    System.out.println("Código Postal: ");
+                    postalCode = sc.next();
+                    System.out.println("País: ");
+                    pais = sc.next();
+                    System.out.println("CPF: ");
+                    cpf = sc.next();
+                    System.out.println("Passaporte: ");
+                    passaporte = sc.next();
+                    System.out.println("Data de Nascimento: ");
+                    dtNascimento = sc.next();
 
+                    Cliente cliente = new Cliente(id, nome, endereco, postalCode, pais, cpf, passaporte, dtNascimento);
+                    CRUD.ClienteInserir(clientes,cliente);
+                    break;
+                case 2:
+                    System.out.println("Digite o id do Cliente que deseja remover: ");
+                    id = sc.nextInt();
+                    CRUD.ClienteRemover(clientes,id);;
+                    break;
+                case 3:
+                    System.out.println("Digite o id do Cliente que deseja alterar: ");
+                    id = sc.nextInt();
+                    System.out.println("DIGITE NULL SE DESEJA NÃO ALTERAR UM ATRIBUTO!");
+                    System.out.println("Digite o Nome: ");
+                    nome = sc.next();
+                    System.out.println("Endereço: ");
+                    endereco = sc.next();
+                    System.out.println("Código Postal: ");
+                    postalCode = sc.next();
+                    System.out.println("País: ");
+                    pais = sc.next();
+                    System.out.println("CPF: ");
+                    cpf = sc.next();
+                    System.out.println("Passaporte: ");
+                    passaporte = sc.next();
+                    System.out.println("Data de Nascimento: ");
+                    dtNascimento = sc.next();
+                    CRUD.ClienteAlterar(clientes,id,nome,endereco,postalCode,pais,cpf,passaporte,dtNascimento);
+                    break;
+                case 4:
+                    CRUD.Cl(clientes);
+                    break;
+                default:
+                    break;
+            }
+        }while (opcao != 5);
     }
 }
